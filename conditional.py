88a344433cfb8badd2b7f439aa1f6433a71c4f4a
Shashant Shekhar <shashantshekhar10@gmail.com>
2025-11-17 00:48:54 +0530

    commited some code

---
+++ conditional.py
@@ -0,0 +1,89 @@
+## conditional statement :-
+# => A conditional statement is used to perform different actions based on whether a certain condition is true or false. It typically involves
+
+# two conditions :- if , else
+
+
+if Rohit reached the class on time:
+                    print("Rohit will attend the class")
+else:
+                    print("Rohit will not attend the class")
+
+
+
+# NOte: ":" colon => it is used to define the body of any statement in python.
+
+# a = 40
+# b = 30
+
+# if a > b:
+#          print("a is greater than b")
+#          print("it is code for if block")
+#          print("another statement in if block")
+
+# else:
+#     print("b is greater than a")
+#     print("it is code for else block")
+#     print("another statement in else block")
+
+# print("hii, guys, this is out of if else block")
+
+
+# NOtes : in python we use colon : to define the body of if and else, any loop.
+
+# in c language :-
+
+# in any statement we use curly braces { } to define the body of  if and else,  any loop.
+
+# Semicolon ; is used to terminate any statement in c language.
+
+# if (a > b)
+# {
+#    printf("a is greater than b");
+#    print("it is code for if block");
+#    print("another statement in if block");
+# }
+
+# else
+# {
+#   printf("b is greater than a");
+#   print("it is code for else block");
+# }
+
+
+
+# print("Indentation in python")
+# print("the is the code for testing indentation in python")
+
+#  print("hello")
+
+# NOtes : Indentation is very important in python.
+# => backbone of python 
+
+
+# if condition is true :
+#                     print("it will print this line of code  this")
+# else:
+#                     print("it will print this line of code  that")
+
+# num = int(input("Enter any number: "))
+
+# if num == 99:
+#     print("you have entered 99")
+# else:
+#     print("you have not entered 99")
+                               
+
+           
+
+# my_number = 777
+
+
+# if (my_number < 500):
+#     print("bhai ye print hone chahiye agr condition true hai to")
+# else:
+#     print("bhai ye print hone chahiye agr condition false hai to")
+
+
+
+# NOte: we can keep our condition in parenthesis () but it is not mandatory in python.
---
+++ indentation.py
@@ -0,0 +1,17 @@
+print("Hello, World!")
+print("Welcome to Python Programming")
+print("Python is easy to learn")
+print("Python is Interpreted Language")
+print("Python is Object Oriented Programming Language")
+print("Python is Dynamically Typed Language")
+print("Python is High Level Language")
+print("Python is Portable Language")
+
+## Indentation in Python:-
+# => Indentation is the space at the beginning of a code line.
+# => Indentation is backbone of Python programming language.
+
+## Comments in Python:-
+# => Comments are the lines which are ignored by the interpreter.
+# => it is just like non-readable code 
+# => it is used to explain the code.
